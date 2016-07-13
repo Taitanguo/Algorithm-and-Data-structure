da004b212f6c35cdd4cec691f6f41bb0d34879c1
SIYUAN GUO <uvasiyuan@gmail.com>
2016-07-13 10:53:34 -0400

    modeified with traditional comding style

--- Algorithm and Datastructure/identical binary tree.java
+++ Algorithm and Datastructure/identical binary tree.java
@@ -11,19 +11,34 @@
  */
 
 //use recursion solve this problem
-public class Solution {
-    /**
-     * @param a, b, the root of binary trees.
-     * @return true if they are identical, or false.
-     */
-    public boolean isIdentical(TreeNode a, TreeNode b) {
-        // Write your code here
-        if (a == null && b == null)
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution{
+    public boolean isIdentical(TreeNode a, TreeNode b){
+        //reversive judgement 
+        if(a == null && b == null){
             return true;
-        if (a != null && b != null) {
-            return a.val == b.val && isIdentical(a.left, b.left)
-                    && isIdentical(a.right, b.right);
         }
+        if(a == null || b == null){
+            return false;
+        }
+        if(a.val != b.val){
+            return false;
+        }
+        //reversive part
+        if(isIdentical(a.left, b.left) && isIdentical(a.right, b.right)){
+            return true;
+        }
+        
         return false;
     }
 }
